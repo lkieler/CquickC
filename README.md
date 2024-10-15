6d502cbf9f25562279ee268e2c16d0e8aa35864c
Lorenz Kieler <lkieler@tsn.at>
2024-10-15 18:45:54 +0200

    Updated README, fixed a bug where the install script would overwrite Files in /home/lorenz/.cargo/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/lorenz/.local/bin/:/home/lorenz/.local/bin/, if they have the same name as in bin

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 # Overview
 
 CquickC is a *very simple* bash script to compile c Programms *just a little faste*.
+(This Script is by no means intended to be used in any Production/Professional enviroments.)
+
+# Usage
+
+Once install you should be able to simply execute `c` in any directory. The script should automatically find any .c files and compile and run them. If multpile files are found it will provide a list to choose from.
+
+If you want to change the name of the command you simply have to rename the file before installing. If you already installed the script or if you used the Manual method you can also just rename it in `/usr/local/bin/`. (might require sudo)
 
 # Installation
 
 ## Using the Install Script
 
 1. Clone the Repo and cd into it
-2. Run the ``install.sh`` file
+2. Run the `install.sh` file
 
 ## Alternative: Manually move the Files
 
 1. Clone the Repo
-2. Move the file(s) from the bin folder into `PATH`
+2. Move the file(s) from the bin folder into `$PATH`
 3. Make the file(s) executable using `chmod +x`
--- install.sh
+++ install.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# Define variables
-SCRIPT_NAME="bin/c"  # Path to your script inside the bin folder
-INSTALL_DIR="/usr/local/bin"     # Default installation directory
-LICENSE_FILE="LICENSE"           # Path to the LICENSE file
+SCRIPT_NAME="bin/c"
+INSTALL_DIR="/usr/local/bin"
+LICENSE_FILE="LICENSE"
 
-# Check if the script exists
 if [[ ! -f "$SCRIPT_NAME" ]]; then
     echo "Error: $SCRIPT_NAME not found in the bin folder."
     exit 1
 fi
 
-# Check if the LICENSE file exists
 if [[ ! -f "$LICENSE_FILE" ]]; then
     echo "Error: LICENSE file not found."
     exit 1
 fi
-
-# Inform the user about the license and ask for acceptance
 echo "The license for this software can be found at $LICENSE_FILE."
 echo "You must read and accept the license before proceeding."
 read -p "Do you accept the terms of the LICENSE? (y/n): " ACCEPT
@@ -27,21 +22,24 @@ if [[ "$ACCEPT" != "y" ]]; then
     exit 1
 fi
 
-# Ask for sudo privileges if not running as root
 if [[ $EUID -ne 0 ]]; then
     echo "Root privileges are required to install the script."
     sudo echo ""  # Ensure sudo is prompted early
 fi
 
-# Copy the script to the installation directory
 SCRIPT_BASENAME=$(basename "$SCRIPT_NAME")  # Get the actual script file name
 echo "Installing $SCRIPT_BASENAME to $INSTALL_DIR"
+
+if [[ -f "$INSTALL_DIR/$SCRIPT_BASENAME" ]]; then
+    echo "Error: A file named '$SCRIPT_BASENAME' already exists in $INSTALL_DIR."
+    echo "Installation aborted to prevent overwriting the existing file."
+    exit 1
+fi
+
 sudo cp "$SCRIPT_NAME" "$INSTALL_DIR"
 
-# Make the script executable
 sudo chmod +x "$INSTALL_DIR/$SCRIPT_BASENAME"
 
-# Confirm installation
 if [[ -f "$INSTALL_DIR/$SCRIPT_BASENAME" ]]; then
     echo "Installation successful! You can now run the script using '$SCRIPT_BASENAME'."
 else
